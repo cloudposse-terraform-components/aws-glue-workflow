0bf77cf7ffba7d5f9debca2d7f9ce5d7a81aebb0
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:35 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "workflow_id" {
+  description = "Glue workflow ID"
+  value       = module.glue_workflow.id
+}
+
+output "workflow_name" {
+  description = "Glue workflow name"
+  value       = module.glue_workflow.name
+}
+
+output "workflow_arn" {
+  description = "Glue workflow ARN"
+  value       = module.glue_workflow.arn
 }
